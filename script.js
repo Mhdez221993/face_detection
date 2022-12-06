ada844737fc6e25776059261234ff0e6e66ef09c
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-06 18:27:19 +0100

    Add event listener to load the face expresions

--- script.js
+++ script.js
@@ -16,5 +16,11 @@ function startVideo() {
 }
 
 video.addEventListener("play", () => {
-  console.log("started");
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    console.log(detections);
+  }, 100);
 });
